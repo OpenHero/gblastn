d2dc302614212d3f6c982f5e2fa2861e7a786afe
openhero <zhao.kaiyong@gmail.com>
2019-10-08 00:38:48 +0800

    Changes to be committed: 	modified:   shell/g.m.sh 	modified:   shell/m.ls

--- shell/g.m.sh
+++ shell/g.m.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "***************start*****************"
-blastn="/home/blastn/bin/gblastn"
-data_dir="/home/blastn/"
-database="${data_dir}database/blastdb/mouse/mouse.wm"
-maskdb="${data_dir}wm_counts/mouse.stat"
+blastn="/home/kyle/testGBTN/gblastn/c++/GCC700-ReleaseMT64/bin/blastn"
+data_dir="/home/kyle/ncbi/"
+database="${data_dir}database/blastdb/human.wm"
+maskdb="${data_dir}database/blastdb/human.stat"
 query_list="m.ls"
-outpath="/home/blastn/output/log.txt"
+outpath="${data_dir}log/log.txt"
  
-$blastn -db $database -window_masker_db $maskdb -query_list $query_list -outfmt "7" -out $outpath -use_gpu true
+$blastn -db $database -window_masker_db $maskdb -query_list $query_list -evalue 1e-5 -max_target_seqs 10 -outfmt "7" -out $outpath -use_gpu true
 
 echo "***************complete*****************"
--- shell/m.ls
+++ shell/m.ls
@@ -1,2 +1,2 @@
-/home/blastn/query/queries/mouse/qmedium/query.195.fa
-/home/blastn/query/queries/mouse/qmedium/query.130.fa
+/home/kyle/ncbi/query/query.10.fa
+/home/kyle/ncbi/query/query.12.fa
